68db6eef524e13dd335c4f3a1dd4bb8e6d458a6e
Nick Lemmon <nick.lemmon@gmail.com>
2025-03-08 07:22:01 -0500

    Design updates

--- src/components/category-link.tsx
+++ src/components/category-link.tsx
@@ -5,7 +5,7 @@ export function CategoryLink({ className, ...props }: { className?: string } & L
   return (
     <Link
       className={cn(
-        'h-52 w-full justify-center items-center bg-gradient-to-b from-60% to-indigo-100 from-indigo-50 hover:from-indigo-100 text-indigo-600 shadow-md shadow-indigo-100/50 border border-indigo-200 p-8 focus-visible:inset-ring-4  inset-ring-indigo-700 inline-flex rounded-2xl text-md font-semibold transition',
+        'h-36 w-full justify-center items-center bg-gradient-to-b from-60% to-indigo-100 from-indigo-50 hover:from-indigo-100 text-indigo-600 shadow-md shadow-indigo-100/50 border border-indigo-200 focus-visible:inset-ring-4  inset-ring-indigo-700 inline-flex rounded-3xl text-md font-semibold transition',
         className,
       )}
       {...props}
