84a7b88b81017f8290f4d08881f1320af40ba25e
Nick Lemmon <nick.lemmon@gmail.com>
2026-03-15 14:30:40 -0400

    Migrates to oxlint, adds build compression (#53)

--- src/components/sr-only.tsx
+++ src/components/sr-only.tsx
@@ -1,5 +1,5 @@
 import type React from 'react'
 
 export function SrOnly({ children }: { children: React.ReactNode }) {
-  return <div className="sr-only">{children}</div>
+return <div className="sr-only">{children}</div>
 }
